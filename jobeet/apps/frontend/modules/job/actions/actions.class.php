b175877d06809eb87c418f40b926c5fa53d03ada
Enri Fonseca <enri.nfonseca@gmail.com>
2016-08-30 17:10:24 -0300

    Limpiando codigo

--- jobeet/apps/frontend/modules/job/actions/actions.class.php
+++ jobeet/apps/frontend/modules/job/actions/actions.class.php
@@ -13,7 +13,6 @@ class jobActions extends sfActions
   public function executeIndex(sfWebRequest $request)
   {
     $this->categories = Doctrine_Core::getTable('JobeetCategory')->getWithJobs();
-    //$this->category = $this->getRoute()->getObject();
   }
 
   public function executeShow(sfWebRequest $request)
@@ -39,21 +38,11 @@ class jobActions extends sfActions
 
   public function executeEdit(sfWebRequest $request)
   {
-    /*$this->forward404Unless($jobeet_job = Doctrine_Core::getTable('JobeetJob')->find(array($request->getParameter('id'))), sprintf('Object jobeet_job does not exist (%s).', $request->getParameter('id')));
-    $this->form = new JobeetJobForm($jobeet_job);*/
     $this->form = new JobeetJobForm($this->getRoute()->getObject());
   }
 
   public function executeUpdate(sfWebRequest $request)
   {
-    /*$this->forward404Unless($request->isMethod(sfRequest::POST) || $request->isMethod(sfRequest::PUT));
-    $this->forward404Unless($jobeet_job = Doctrine_Core::getTable('JobeetJob')->find(array($request->getParameter('id'))), sprintf('Object jobeet_job does not exist (%s).', $request->getParameter('id')));
-    $this->form = new JobeetJobForm($jobeet_job);
-
-    $this->processForm($request, $this->form);
-
-    $this->setTemplate('edit');*/
-
     $this->form = new JobeetJobForm($this->getRoute()->getObject());
     $this->processForm($request, $this->form);
     $this->setTemplate('edit');
@@ -61,12 +50,6 @@ class jobActions extends sfActions
 
   public function executeDelete(sfWebRequest $request)
   {
-    /*$request->checkCSRFProtection();
-
-    $this->forward404Unless($jobeet_job = Doctrine_Core::getTable('JobeetJob')->find(array($request->getParameter('id'))), sprintf('Object jobeet_job does not exist (%s).', $request->getParameter('id')));
-    $jobeet_job->delete();
-
-    $this->redirect('job/index');*/
     $request->checkCSRFProtection();
 
     $job = $this->getRoute()->getObject();
@@ -86,13 +69,6 @@ class jobActions extends sfActions
 
   protected function processForm(sfWebRequest $request, sfForm $form)
   {
-    /*$form->bind($request->getParameter($form->getName()), $request->getFiles($form->getName()));
-    if ($form->isValid())
-    {
-      $jobeet_job = $form->save();
-
-      $this->redirect('job/edit?id='.$jobeet_job->getId());
-    }*/
     $form->bind(
       $request->getParameter($form->getName()),
       $request->getFiles($form->getName())
