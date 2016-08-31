cf506a05afd2c9ced5bfcc32a41e15e4e3773a6f
Enri Fonseca <enri.nfonseca@gmail.com>
2016-08-31 15:44:02 -0300

    Agregando metodo para resetear historial de jobs

--- jobeet/apps/frontend/lib/myUser.class.php
+++ jobeet/apps/frontend/lib/myUser.class.php
@@ -3,15 +3,19 @@
 class myUser extends sfBasicSecurityUser
 {
 	public function addJobToHistory(JobeetJob $job){
-		$jobs = $this->getAttribute('job_historys', array());
+		$jobs = $this->getAttribute('job_history', array());
 
 		if(!in_array($job, $jobs)){
 			array_unshift($jobs, $job);
-			$this->setAttribute('job_historys', array_slice($jobs, 0, 3));
+			$this->setAttribute('job_history', array_slice($jobs, 0, 3));
 		}
 	}
 
 	public function getJobsHistory(){
-		return $this->getAttribute('job_historys', array());
+		return $this->getAttribute('job_history', array());
+	}
+
+	public function resetJobHistory(){
+		$this->getAttributeHolder()->remove('job_history');
 	}
 }
