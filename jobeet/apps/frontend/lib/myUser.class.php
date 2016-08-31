90242b1da034266855cead93c139f471f1439472
Enri Fonseca <enri.nfonseca@gmail.com>
2016-08-31 15:41:18 -0300

    Historial de jobs visitados

--- jobeet/apps/frontend/lib/myUser.class.php
+++ jobeet/apps/frontend/lib/myUser.class.php
@@ -2,4 +2,16 @@
 
 class myUser extends sfBasicSecurityUser
 {
+	public function addJobToHistory(JobeetJob $job){
+		$jobs = $this->getAttribute('job_historys', array());
+
+		if(!in_array($job, $jobs)){
+			array_unshift($jobs, $job);
+			$this->setAttribute('job_historys', array_slice($jobs, 0, 3));
+		}
+	}
+
+	public function getJobsHistory(){
+		return $this->getAttribute('job_historys', array());
+	}
 }
